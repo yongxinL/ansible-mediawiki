368e23dbd267522227f4605739b7841ce5f57a77
yongxinL <yongxinL@outlook.com>
2017-08-24 10:19:36 +1000

    - change script and use MySQL batch to create / modify database and user - add full path for wikidb_dump file

--- files/mediawiki-prepareMySQL.sh
+++ files/mediawiki-prepareMySQL.sh
@@ -28,6 +28,10 @@ if [ ! -f "${script_path}/LocalSettings.php" ]; then
     exit_fail "Please run this script in MediaWiki root directory! "
 fi
 
+if [ -f "${script_path}/preparedb.sql" ]; then
+	rm -f "${script_path}/preparedb.sql"
+fi
+
 # retrieve DB user/password
 db_name=$(sed -n 's/^$wgDBname = "\([^"]*\)";/\1/p' ${script_path}/LocalSettings.php)
 db_user=$(sed -n 's/^$wgDBuser = "\([^"]*\)";/\1/p' ${script_path}/LocalSettings.php)
@@ -41,14 +45,30 @@ if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]
 
     echo "Please enter root user MySQL password!"
     read rootpasswd
-    print_info "Create user and database ..."
-    mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${wgDBname} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-    mysql -uroot -p${rootpasswd} -e "CREATE USER ${wgDBuser}@localhost IDENTIFIED BY '${wgDBpasswd}';"
-    mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${wgDBname}.* TO '${wgDBuser}'@'localhost';"
-    mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
-    if [ -f "${_self_root}/dbs/wikidb_dump.sql" ]; then
+    print_info "Create wiki database ${db_name} and user ${db_user} ..."
+    echo "/* - drop database if exists. */" > ${script_path}/preparedb.sql
+    echo "DROP DATABASE IF EXISTS ${db_name};" >> ${script_path}/preparedb.sql
+    echo "/* - create database if not exists. */" >> ${script_path}/preparedb.sql
+    echo "CREATE DATABASE IF NOT EXISTS ${db_name} /*\!40100 DEFAULT CHARACTER SET utf8 */;" >> ${script_path}/preparedb.sql
+    echo "/* - there is not create user if not exist command, " >> ${script_path}/preparedb.sql
+    echo "     but grant privileges will create user first if user doesn't exist! */" >> ${script_path}/preparedb.sql
+    echo "GRANT ALL PRIVILEGES ON ${db_name}.* TO '${db_user}'@'localhost';" >> ${script_path}/preparedb.sql
+    echo "/* - reset user password. */" >> ${script_path}/preparedb.sql
+    echo "UPDATE mysql.user" >> ${script_path}/preparedb.sql
+    echo "    SET authentication_string = PASSWORD('${db_pass}'), password_expired = 'N'" >> ${script_path}/preparedb.sql
+    echo "    WHERE User = '${db_user}' AND Host = 'localhost';" >> ${script_path}/preparedb.sql
+    echo "/* - flush privileges. */" >> ${script_path}/preparedb.sql
+    echo "FLUSH PRIVILEGES;" >> ${script_path}/preparedb.sql
+    mysql -uroot -p${rootpasswd} < ${script_path}/preparedb.sql
+    if [ $? -ne 0 ]; then
+    	exit_fail
+    else
+    	rm -f ${script_path}/preparedb.sql
+    fi
+
+    if [ -f "${script_path}/dbs/wikidb_dump.sql" ]; then
         print_info "import database backup ..."
-        mysql -uroot -p${rootpasswd} ${db_name} < dbs/wikidb_dump.sql
+        mysql -uroot -p${rootpasswd} ${db_name} < ${script_path}/dbs/wikidb_dump.sql
         php ${script_path}/maintenance/update.php --quick
     fi
 
